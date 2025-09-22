9614fbeadc6f225ef4ac967a3c62145fafb2ec42
Ravel G. S. Cruz <devravelcruz@gmail.com>
2025-09-22 04:49:54 -0300

    Ajustes para responsividade

--- script.js
+++ script.js
@@ -97,13 +97,3 @@ document.addEventListener("DOMContentLoaded", () => {
     banner.style.display = "none";
   });
 });
-
-// ===== MENU HAMBURGUER =====
-const hamMenu = document.querySelector(".ham-menu");
-
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
